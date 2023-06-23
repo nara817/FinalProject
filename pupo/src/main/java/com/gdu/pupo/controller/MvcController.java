3b291122fdb2c551a776a4ac07f587301b4797f2
tikitaca <sweet3761@naver.com>
2023-06-23 16:37:28 +0900

    2023-06-23-jo

--- pupo/src/main/java/com/gdu/pupo/controller/MvcController.java
+++ pupo/src/main/java/com/gdu/pupo/controller/MvcController.java
@@ -11,9 +11,4 @@ public class MvcController {
 		return "index";  // src/main/resources/templates/index.html
 	}
 	
-	@GetMapping("theme/admin/modules-sweet-alert.html")
-  public String admin() {
-    return "theme/admin/modules-sweet-alert";  // src/main/resources/templates/index.html
-  }
-	
 }
